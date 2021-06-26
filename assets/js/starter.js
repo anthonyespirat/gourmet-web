8ca03b5084bd6a58c844ac52aaaae4a05b8cd9b4
noxtv1 <72048119+noxtv1@users.noreply.github.com>
2021-06-26 22:01:47 +0200

    initial commit

---
+++ assets/js/starter.js
@@ -0,0 +1,41 @@
+import "../../node_modules/jquery/dist/jquery.js"
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.js";
+// Importing JavaScript
+//
+// You have two choices for including Bootstrap's JS files—the whole thing,
+// or just the bits that you need.
+
+
+// Option 1
+//
+// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
+
+// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+
+// Option 2
+//
+// Import just what we need
+
+// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
+// import "../../node_modules/popper.js/dist/popper.min.js";
+
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      if (this.hash !== "") {
+        event.preventDefault();
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 900, function(){
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
+
+
+
+
